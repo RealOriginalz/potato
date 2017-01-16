b9f48d2432afd2ca4f0b816740d716b624ce8a9d
Parker <RealOriginalz98+Github@gmail.com>
2017-01-16 12:01:00 -0800

    initial commit

---
+++ Assets/Scripts/Existornot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Existornot : MonoBehaviour {
+	
+	//void Update ()
+	//{
+		//gameObject.SetActive (false); 			//default this object isn't active,
+		//if (GameObject.FindWithTag ("Key") = (false))
+			//gameObject.SetActive (true);
+		//}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	void Update()
+	{
+		processmovement();
+
+	}
+	void processmovement ()
+	{
+		var x = Input.GetAxis("Mouse X") * Time.deltaTime * 150.0f; 	//mouse is getting left and right movement
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f; 		//ws & arrow keys getting forward and backward movement
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+	}
+	void OnTriggerEnter(Collider other) { 
+		Destroy (GameObject.FindWithTag ("Key")); 						//destroy gameobject tagged with "key" upon collision
+		}
+}
